a31ff50443729a4102d6af1abc9a1f688b7ffa56
theapekshya <139621450+theapekshya@users.noreply.github.com>
2023-11-08 22:07:57 +0545

    Date_Object_in_js

---
+++ date.js
@@ -0,0 +1,11 @@
+// Date Object in js
+
+
+let x = new Date();  // gives current date
+console.log(x);
+
+let y = new Date(2018,11,13,10,33,56,7);  // date according to data
+console.log(y);
+
+let z = new Date("October 13,2018 11:12:33"); // gives date up 2018 
+console.log(z.getTime());
